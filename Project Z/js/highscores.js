93dc83d6d8249115b370598ab2149ac4286b099c
29907ilya <59442871+29907ilya@users.noreply.github.com>
2020-09-04 20:26:51 +0300

    Add files

---
+++ Project Z/js/highscores.js
@@ -0,0 +1,48 @@
+var nickName = "";
+
+getName.onclick = function () {
+  var str = document.getElementById("inputName");
+  nickName = str.value;
+  console.log(nickName);
+};
+
+nicktext = nickName;
+
+if (nicktext == "") nicktext = "Player";
+
+var scoreTable = document.querySelector(".tableDiv");
+var bodyHeight = document.body.offsetHeight;
+var bodyWidth = document.body.offsetWidth;
+var scoreTableHeight;
+var scoreTableWidth;
+var tableNone = false;
+
+// ------------ таблица рекордов: узнаем размеры
+scoreTable.style.display = "block";
+
+scoreTableHeight = scoreTable.offsetHeight;
+scoreTableWidth = scoreTable.offsetWidth;
+
+scoreTable.style.top = -scoreTableHeight + "px";
+scoreTable.style.left = bodyWidth / 2 - scoreTableWidth / 2 + "px";
+
+// -------- позиционируем таблицу
+function tableScore() {
+  scoreTable.style.display = "block";
+
+  if (!tableNone) {
+    read();
+    scoreTable.style.top = "50%";
+    scoreTable.style.left = "50%";
+    scoreTable.style.transform =
+      "translateZ(0) translateX(-50%) translateY(-50%)";
+
+    tableNone = true;
+  } else {
+    scoreTable.style.top = "0";
+    scoreTable.style.left = "50%";
+    scoreTable.style.transform =
+      "translateZ(0) translateX(-50%)  translateY(-100%) ";
+    tableNone = false;
+  }
+}
