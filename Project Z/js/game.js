95bdbc0834ccbcfcf4f1f30b71e8d23caea1a96c
29907ilya <59442871+29907ilya@users.noreply.github.com>
2020-09-11 20:18:55 +0300

    Update game.js

--- Project Z/js/game.js
+++ Project Z/js/game.js
@@ -1,50 +1,56 @@
 function startGame() {
-  $("#start").hide();
-  $("#game").show();
-  game();
-  clickSound(mainSound);
-}
+    clickSound(mainSound);
+    $("#start").hide();
+    $("#game").show();
+    game();
+  }
 
-function gameOver() {
-  console.log("gameOver");
-  $("#game-over").show();
-}
+  $(window).on('beforeunload', function(){ 
+    return; 
+  });
+  
+  function gameOver() {
+    console.log("gameOver");
+    $("#game-over").show();
+  }
+  
+  function mainMenu() {
+    $("#game").hide();
+    $("#start").show();
+    lockGet(randPass);
+  }
+  
+  document.getElementById("play-again").addEventListener("click", toMainMenu)
+  function toMainMenu() {
+    console.log("game-over none");
+    $("#game-over").hide();
+    zombies = [];
+    timer = 0;
+    mainMenu();
+    cancelAnimationFrame(myReq);
+    reset();
+  };
+  
+  var requestAnimationFrame = (function () {
+    return (
+      window.requestAnimationFrame ||
+      window.webkitRequestAnimationFrame ||
+      window.mozRequestAnimationFrame ||
+      window.oRequestAnimationFrame ||
+      window.msRequestAnimationFrame ||
+      function (callback) {
+        window.setTimeout(callback, 1000 / 60);
+      }
+    );
+  })();
 
-function mainMenu() {
-  $("#game").hide();
-  $("#start").show();
-  lockGet(randPass);
-}
-
-document.getElementById("play-again").addEventListener("click", toMainMenu)
-function toMainMenu() {
-  console.log("game-over none");
-  $("#game-over").hide();
-  zombies = [];
-  timer = 0;
-  mainMenu();
-
-  reset();
-};
-
-var requestAnimationFrame = (function () {
-  return (
-    window.requestAnimationFrame ||
-    window.webkitRequestAnimationFrame ||
-    window.mozRequestAnimationFrame ||
-    window.oRequestAnimationFrame ||
-    window.msRequestAnimationFrame ||
-    function (callback) {
-      window.setTimeout(callback, 1000 / 60);
-    }
-  );
-})();
-
-function reset() {
-  document.getElementById("game-over").style.display = "none";
-  newscore = 0;
-  zombies = [];
-  bullets = [];
-  shooter = { x: 420, y: 410, speed: 200, animx: 0, animy: 0 };
-}
-
+  var cancelAnimationFrame = window.cancelAnimationFrame || window.mozCancelAnimationFrame;
+  var myReq;
+  
+  function reset() {
+    document.getElementById("game-over").style.display = "none";
+    newscore = 0;
+    zombies = [];
+    bullets = [];
+    shooter = { x: 420, y: 410, speed: 200, animx: 0, animy: 0 };
+  }
